2038574ec51eb26958fca68ec40afd84c6a61e41
Angelo Evangelista <angeloevan.ane@gmail.com>
2024-07-10 10:44:42 -0300

    feat: add log file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import cors from "cors";
 import express from "express";
+import fs from "fs";
+import path from "path";
 
 const app = express();
+const logFilePath = path.join(__dirname, 'requests.log');
 
 app.use(express.json());
 app.use(
   cors({
-    origin:
-      "*",
+    origin: "*",
   })
 );
+
+app.use((request, response, next) => {
+  const logEntry = {
+    hostname: request.hostname,
+    origin: request.headers.origin,
+    ip: request.headers["x-forwarded-for"] || request.socket.remoteAddress,
+    body: request.body,
+    path: request.path,
+    timestamp: new Date().toISOString()
+  };
+  
+  fs.appendFile(logFilePath, JSON.stringify(logEntry) + '\n', (err) => {
+    if (err) {
+      console.error("Failed to write log entry:", err);
+    }
+  });
+
+  next();
+});
+
+app.get('/logs', (request, response) => {
+  return response.sendFile(logFilePath);
+});
 
 app.all("/*", (request, response) => {
   console.dir(
@@ -25,6 +50,7 @@ app.all("/*", (request, response) => {
 
   return response.status(204).send();
 });
+
 
 const port = process.env.PORT || 3333;
 
